41a8974316f33c81859a3495a79f6dfb81fa235e
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2023-05-13 13:34:54 +0300

    Adding models

---
+++ futArabicom/Areas/Identity/Data/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace futArabicom.Areas.Identity.Data
+{
+    public class Comment
+    {
+        public string Content { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
