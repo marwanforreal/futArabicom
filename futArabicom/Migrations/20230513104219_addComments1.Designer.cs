1d537a86f027a534dbe4100c4788a673fc1c029d
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2023-05-13 13:46:09 +0300

    Add table of comments

--- futArabicom/Migrations/20230513104219_addComments1.Designer.cs
+++ futArabicom/Migrations/20230513104219_addComments1.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using futArabicom.Data;
 
@@ -10,9 +11,11 @@ using futArabicom.Data;
 namespace futArabicom.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230513104219_addComments1")]
+    partial class addComments1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
