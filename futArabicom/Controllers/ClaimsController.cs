1f58b0df4b3bf884690bc67414d71c57eb1e20a4
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2023-10-28 22:48:48 +0300

    add claim

--- futArabicom/Controllers/ClaimsController.cs
+++ futArabicom/Controllers/ClaimsController.cs
@@ -1,10 +1,19 @@
 ﻿using futArabicom.Areas.Identity.Data;
+using futArabicom.Data;
+using futArabicom.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace futArabicom.Controllers
 {
     public class ClaimsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClaimsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,13 +21,41 @@ namespace futArabicom.Controllers
 
         public IActionResult Create(int playerId)
         {
+            TempData["playerId"] = playerId; 
+
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(Claims claim)
         {
-            return RedirectToAction("Details", new { id = claim.Player.Id });
+            var playerIdString = TempData["playerId"].ToString();
+
+            int playerId = 0; 
+
+            int.TryParse(playerIdString, out playerId);
+
+            var playerModel = getPlayerById(playerId);
+
+            claim.Player = playerModel;
+
+            claim.isVerified = false; 
+
+            claim.timeStamp = DateTime.UtcNow;
+
+            claim.User = getUserByUsername(User.Identity.Name);
+
+            return RedirectToAction("Details", "Players", new { id = claim.Player.Id });
+        }
+
+        private Player getPlayerById(int playerId)
+        {
+            return _context.Players.SingleOrDefault(p => p.Id == playerId);
+        }
+
+        private User getUserByUsername(string username)
+        {
+            return _context.Users.FirstOrDefault(x => x.UserName == username);
         }
     }
 }
